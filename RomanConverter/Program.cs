3be13133a997e693978c548f43ee9af3c2706bb7
ewaight12 <ewaight12@gmail.com>
2017-06-16 12:30:22 -0400

    Added RomanCoverter

---
+++ RomanConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RomanConverter;
+
+namespace RomanConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the Roman Numeral you would like to convert:");
+            string romanNumeral = Console.ReadLine();
+
+            RomanConverter converter = new RomanConverter();
+           
+            int integer = converter.ConvertRomans(romanNumeral);
+
+            Console.WriteLine(integer);
+        }
+    }
+}
---
+++ RomanConverter/RomanConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+namespace RomanConverter
+{
+    public class RomanConverter
+    {
+        public int ConvertRomans(string roman)
+        {
+            //Variables
+            Dictionary<char, int> lookup = new Dictionary<char, int>();
+            lookup.Add('I', 1);
+            lookup.Add('V', 5);
+            lookup.Add('X', 10);
+            lookup.Add('L', 50);
+            lookup.Add('C', 100);
+            lookup.Add('D', 500);
+            lookup.Add('M', 1000);
+
+            int integer = 0;
+            int lastVal = 0;
+
+            //Convert Romans to Integers
+            for (int i = roman.Length - 1; i >= 0; i--)
+            {
+                int value = lookup[roman[i]];
+
+                if (value < lastVal){
+                    integer -= value;
+                }
+
+                else 
+                {
+                    integer += value;
+                    lastVal = value;
+                }
+            }
+            return integer;
+
+        }
+    }
+}
